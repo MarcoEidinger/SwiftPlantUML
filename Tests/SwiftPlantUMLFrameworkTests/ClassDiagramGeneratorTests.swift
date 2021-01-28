ebdeb59c2b788ec75c40e9786fad103416bce6f6
Marco Eidinger <marco.eidinger@sap.com>
2021-01-27 16:18:46 -0800

    feat: 🎸 generate diagram from a string containing Swift code
    can be used in Xcode Source Editor Extension

--- Tests/SwiftPlantUMLFrameworkTests/ClassDiagramGeneratorTests.swift
+++ Tests/SwiftPlantUMLFrameworkTests/ClassDiagramGeneratorTests.swift
@@ -8,6 +8,13 @@ struct MockPlantUMLPresenter: PlantUMLPresenting {
     }
 }
 
+struct MockPlantUMLPresenterForTestFromString: PlantUMLPresenting {
+    func present(script: PlantUMLScript, completionHandler: @escaping () -> Void) {
+        XCTAssertTrue(script.text.contains("class \"Test\""))
+        completionHandler()
+    }
+}
+
 final class ClassDiagramGeneratorTests: XCTestCase {
     func testNofiles() {
         let generator = ClassDiagramGenerator()
@@ -22,5 +29,10 @@ final class ClassDiagramGeneratorTests: XCTestCase {
     func testDotAlias() {
         let generator = ClassDiagramGenerator()
         generator.generate(for: ["."], presentedBy: MockPlantUMLPresenter())
+    }
+
+    func testFromString() {
+        let generator = ClassDiagramGenerator()
+        generator.generate(from: "struct Test {}", presentedBy: MockPlantUMLPresenterForTestFromString())
     }
 }
