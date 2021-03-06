fd877cae343459fed3c6431ea0b8592b681d5434
Marco Eidinger <marco.eidinger@sap.com>
2021-03-06 15:18:58 -0800

    refactor: 💡 support `debug` log statement
    CLI logs in console per default but if diagram is printed into console
    then log into a file
    
    BREAKING CHANGE: 🧨 SwiftPlantUMLFramework drops its `ConsoleLogger` implementation.
    Consumers have to bring their own logger implementation! FYI:
    swiftplantuml as command-line tool is using `SwiftyBeaver` for logging.

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     dependencies: [
         .package(name: "SourceKitten", url: "https://github.com/jpsim/SourceKitten", from: "0.31.0"),
         .package(name: "swift-argument-parser", url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.1")),
+        .package(name: "SwiftyBeaver", url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .exact("1.9.3")),
     ],
     targets: [
         .target(
@@ -22,6 +23,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 "SwiftPlantUMLFramework",
+                .product(name: "SwiftyBeaver", package: "SwiftyBeaver"),
             ]
         ),
         .target(
--- Tests/SwiftPlantUMLFrameworkTests/Logger+Tests.swift
+++ Tests/SwiftPlantUMLFrameworkTests/Logger+Tests.swift
@@ -6,22 +6,6 @@ final class LoggerTests: XCTestCase {
         XCTAssertNotNil(Logger.shared)
     }
 
-    func testConsoleLoggerQuit() {
-        let cl = ConsoleLogger(verbose: false)
-        XCTAssertEqual(cl.logLevel, .error)
-        cl.error("--1")
-        cl.warning("--2")
-        cl.info("--3")
-    }
-
-    func testConsoleLoggerVerbose() {
-        let cl = ConsoleLogger(verbose: true)
-        XCTAssertEqual(cl.logLevel, .debug)
-        cl.error("--1v")
-        cl.warning("--2v")
-        cl.info("--3v")
-    }
-
     func testNoLogger() {
         let nl = NoLogger()
         XCTAssertNotNil(nl)
