123f66b2d8185e8af3cf8c63ca0bfa66fefadc53
Marco Eidinger <eidingermarco@gmail.com>
2023-01-13 14:09:47 -0800

    ✨ feat: open diagram as SVG in browser through PlantUMLFramework
    option not included in command-line tool

--- Sources/SwiftPlantUMLFramework/Presenting/PlantUMLBrowserPresenter.swift
+++ Sources/SwiftPlantUMLFramework/Presenting/PlantUMLBrowserPresenter.swift
@@ -4,7 +4,12 @@ import Foundation
 /// presentation formats supported to render PlantUML scripts in browser
 public enum BrowserPresentationFormat {
     /// image only (as .png)
+    @available(*, deprecated, message: "Use .png instead")
     case imagePng
+    /// image only (as .png)
+    case png
+    /// image only (as .svg)
+    case svg
     /// editable script and corresponding diagram
     case `default`
 }
@@ -28,8 +33,10 @@ public struct PlantUMLBrowserPresenter: PlantUMLPresenting {
         let encodedText = script.encodeText()
         let url: URL!
         switch format {
-        case .imagePng:
+        case .imagePng, .png:
             url = URL(string: "https://www.planttext.com/api/plantuml/png/\(encodedText)")
+        case .svg:
+            url = URL(string: "https://www.planttext.com/api/plantuml/svg/\(encodedText)")
         default:
             url = URL(string: "https://www.planttext.com/?text=\(encodedText)")!
         }
